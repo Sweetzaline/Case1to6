b061e0f1fcbbc573c91a7190b67c011c9053efb4
unknown <buragohain4@gmail.com>
2022-07-28 17:31:26 +0530

    Implementing UC1

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constants
+            int IS_WORKING = 1;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_WORKING)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+            }
+        }
+    }
+}
