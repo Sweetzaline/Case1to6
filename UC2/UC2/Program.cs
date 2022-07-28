5f5bf910faaf8507ef58e79f73082d733ac8fa06
unknown <buragohain4@gmail.com>
2022-07-28 17:53:13 +0530

    Implementing UC2

---
+++ UC2/UC2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Question: Calculate Daily Employee Wage");
+            //Constants
+            int PRESENT = 1;
+            int EMP_WAGE_PER_HOUR = 500;
+            //variables
+            int empHr = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == PRESENT)
+            {
+                empHr = 8;
+                Console.WriteLine("Employee is Present Today. ");            }
+            else
+            {
+                empHr = 0;
+                Console.WriteLine("Employee is Absent Today. ");
+            }
+            empWage = empHr * EMP_WAGE_PER_HOUR;
+            Console.WriteLine("Employee's Daily Wage is : " + empWage);
+        }
+    }
+}
